f063b8fccd0ca140a594c28315c9b1a884263862
Karolina <karolina.m.zrobek@gmail.com>
2023-04-21 19:28:48 +0900

    stage

---
+++ src/Pictures/ErrorPage.js
@@ -0,0 +1,8 @@
+<div className="error-page">
+  <h1>Oops! Something went wrong.</h1>
+  <p>We're sorry, but an unexpected error occurred.</p>
+  <button onClick={() => window.location.href = "/"}>Return to Home Page</button>
+</div>
+
+
+
--- web_app/src/Pictures/ErrorPage.js
+++
@@ -1,8 +0,0 @@
-<div className="error-page">
-  <h1>Oops! Something went wrong.</h1>
-  <p>We're sorry, but an unexpected error occurred.</p>
-  <button onClick={() => window.location.href = "/"}>Return to Home Page</button>
-</div>
-
-
-
